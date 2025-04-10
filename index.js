f7c1bff0fc9b4f18ec49000947a14e7dc418c0b0
Shree Dhamija <shreedhamija@gmail.com>
2025-04-10 14:36:06 +0530

    ig connect debugging 7

--- index.js
+++ index.js
@@ -505,7 +505,7 @@ function buildImageCreativePayload({ adName, adSetId, pageId, imageHash, cta, li
       creative: {
         object_story_spec: {
           page_id: pageId,
-          ...(instagramAccountId && { instagram_user_id: instagramAccountId }),
+          instagram_user_id: "9673673102661593",
           link_data: {
             name: headlines[0],
             description: descriptionsArray[0],
