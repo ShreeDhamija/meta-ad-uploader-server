2b7ddd725fd9a13538265d7e40de65cdee35efd9
Shree Dhamija <shreedhamija@gmail.com>
2025-04-24 14:54:07 +0530

    redis again lol 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const {
 } = require("./firebaseController");
 
 const { createClient } = require('redis');
-const ConnectRedis = require('connect-redis').default;
+const ConnectRedis = require('connect-redis');
 
 app.use(express.json());
 app.set('trust proxy', 1);
@@ -1152,10 +1152,10 @@ async function handleDynamicVideoAd(req, token, adAccountId, adSetId, pageId, ad
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 // process.on('SIGINT', async () => {
 //   console.log('Shutting down server...');
